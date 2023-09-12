8a66bc5a97b81f1e53f908f7c28e3a0c65eb8d6a
june0929 <qk1413@naver.com>
2023-09-13 01:26:47 +0900

    :wrench: Setup DB Connection, Todo test API
    Setup DB connection, add Todo entity and test API

---
+++ src/config/database.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export class TypeOrmConfigService {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    };
+  }
+}
