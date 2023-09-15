0f36261faf24c6919650768d3ee67cb7ff2e0eef
junman <junman9595@gmail.com>
2023-09-15 19:14:23 +0900

    temp: erase db related codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,17 @@ import { HealthCheckController } from './health-check/health-check.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TodoController } from './todo/todo.controller';
-import { TodoService } from './todo/todo.service';
-import { TodosEntity } from './todo/todo.entity';
 
 @Module({
   imports: [
     TerminusModule,
     HttpModule,
-    DatabaseModule,
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
     }),
-    TypeOrmModule.forFeature([TodosEntity]),
   ],
-  controllers: [AppController, HealthCheckController, TodoController],
-  providers: [AppService, TodoService],
+  controllers: [AppController, HealthCheckController],
+  providers: [AppService],
 })
 export class AppModule {}
