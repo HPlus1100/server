1515762a154c58ab83c92b115c6abf5ea206d874
unknown <junman9595@gmail.com>
2023-09-25 23:37:29 +0900

    chore: hot reload 추가

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const module: any;
+
 interface EnvironmentVariables {
   PORT: number;
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,5 +17,10 @@ async function bootstrap() {
   const port = configService.get<number>('PORT') || 3000; // 환경 변수에서 PORT 값을 가져옵니다. 없으면 기본값 3000 사용
 
   await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
