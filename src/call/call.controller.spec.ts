0de455c3adf322834d882935ba41b66da17d783c
unknown <junman9595@gmail.com>
2023-09-26 00:27:58 +0900

    fix: provider 미설정으로 인한 test 통과 실패 이슈 해결

--- src/call/call.controller.spec.ts
+++ src/call/call.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CallController } from './call.controller';
+import { CallService } from './call.service';
 
 describe('CallController', () => {
   let controller: CallController;
@@ -7,6 +8,7 @@ describe('CallController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CallController],
+      providers: [CallService],
     }).compile();
 
     controller = module.get<CallController>(CallController);
