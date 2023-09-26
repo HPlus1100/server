e711d71c0e3b07372a4f4d1f2934d87f838a13fc
kthss01 <kthss01@naver.com>
2023-09-26 11:54:44 +0900

    taxi basic crud

--- src/taxi/taxi.controller.ts
+++ src/taxi/taxi.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('taxi')
-export class TaxiController {}
+export class TaxiController {
+  // 전체 택시 정보 조회
+  @Get()
+  getAllTaxiInfo(): string {
+    return 'return all taxi info';
+  }
+
+  // 택시 Id로 택시 정보 조회
+  @Get(':id')
+  getTaxiInfoById(@Param('id') taxiId: string): string {
+    return `return taxi info by id : ${taxiId}`;
+  }
+
+  // 택시 정보 등록
+  createTaxiInfo(): string {
+    return 'create taxi info';
+  }
+
+  // 택시 정보 수정
+  updateTaxiInfo(): string {
+    return 'update taxi info';
+  }
+
+  // 택시 정보 삭제
+  deleteTaxiInfo(): string {
+    return 'delete taxi info';
+  }
+}
