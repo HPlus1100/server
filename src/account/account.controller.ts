8404afe38d82219a03d15bb3ed3a52662222b962
kthss01 <kthss01@naver.com>
2023-09-26 12:21:26 +0900

    taxi controller basic check

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -5,30 +5,51 @@ export class AccountController {
   // 전체 계좌 정보 조회
   @Get()
   getAllAccountInfo(): string {
+    // 사용자 권한 확인
+
     return 'return all account info';
   }
 
   // 계좌 id로 계좌 정보 조회
   @Get(':id')
   getAccountInfoById(@Param('id') accountId: string): string {
+    // 사용자 권한 확인
+    // 계좌 id 존재 확인
+
     return `return account info by id : ${accountId}`;
   }
 
   // 계좌 정보 등록
   @Post()
   createAccountInfo(): string {
+    // 사용자 권한 확인
+    /**
+     * 파라미터 확인
+     */
+    // 계좌 번호 유효성 검사
+    // 계좌 id 생성
+
     return 'create account info';
   }
 
   // 계좌 정보 수정
   @Patch(':id')
   updateAccountInfoById(@Param('id') accountId: string): string {
+    // 사용자 권한 확인
+    /**
+     * 파라미터 확인
+     */
+    // 계좌 id 존재 확인
+
     return `update account info by id : ${accountId}`;
   }
 
   // 계좌 정보 삭제
   @Delete(':id')
   deleteAccountInfoById(@Param('id') accountId: string): string {
+    // 사용자 권한 확인
+    // 계좌 id 존재 확인
+
     return `delete account info by id : ${accountId}`;
   }
 }
