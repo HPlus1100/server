0d2cd26ee9cd25059e272cea2905d83199998453
kthss01 <kthss01@naver.com>
2023-09-26 12:00:38 +0900

    taxi controller basic check

--- src/taxi/taxi.controller.ts
+++ src/taxi/taxi.controller.ts
@@ -1,31 +1,55 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('taxi')
 export class TaxiController {
   // 전체 택시 정보 조회
   @Get()
   getAllTaxiInfo(): string {
+    // 사용자 권한 확인
+
     return 'return all taxi info';
   }
 
   // 택시 Id로 택시 정보 조회
   @Get(':id')
   getTaxiInfoById(@Param('id') taxiId: string): string {
+    // 사용자 권한 확인
+    // 택시 id 존재 확인
+
     return `return taxi info by id : ${taxiId}`;
   }
 
   // 택시 정보 등록
+  @Post()
   createTaxiInfo(): string {
+    // 사용자 권한 확인
+    /**
+     * 파라미터 확인
+     *
+     */
+
     return 'create taxi info';
   }
 
   // 택시 정보 수정
-  updateTaxiInfo(): string {
-    return 'update taxi info';
+  @Patch(':id')
+  updateTaxiInfoById(@Param('id') taxiId: string): string {
+    // 사용자 권한 확인
+    /**
+     * 파라미터 확인
+     *
+     */
+    // 택시 id 존재 확인
+
+    return `update taxi info by id : ${taxiId}`;
   }
 
   // 택시 정보 삭제
-  deleteTaxiInfo(): string {
-    return 'delete taxi info';
+  @Delete(':id')
+  deleteTaxiInfoById(@Param('id') taxiId: string): string {
+    // 사용자 권한 확인
+    // 택시 id 존재 확인
+
+    return `delete taxi info by id : ${taxiId}`;
   }
 }
