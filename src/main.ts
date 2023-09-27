8aa504c31f1d303f07559698cc3669c60017b91a
unknown <junman9595@gmail.com>
2023-09-28 00:43:56 +0900

    chore: hot reload 삭제

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const module: any;
-
 interface EnvironmentVariables {
   PORT: number;
 }
@@ -17,10 +14,5 @@ async function bootstrap() {
   const port = configService.get<number>('PORT') || 3000; // 환경 변수에서 PORT 값을 가져옵니다. 없으면 기본값 3000 사용
 
   await app.listen(port);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
