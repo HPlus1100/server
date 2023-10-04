735a4fab1ea4150da4b971b78c61ae85a45bb26d
june0929 <qk1413@naver.com>
2023-10-04 14:17:18 +0900

    fix : provider 미설정으로 인한 test 통과 실패 이슈 해결

--- src/billing/billing.controller.spec.ts
+++ src/billing/billing.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BillingController } from './billing.controller';
+import { BillingService } from "./billing.service";
 
 describe('BillingController', () => {
   let controller: BillingController;
@@ -7,6 +8,7 @@ describe('BillingController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BillingController],
+      providers: [BillingService]
     }).compile();
 
     controller = module.get<BillingController>(BillingController);
