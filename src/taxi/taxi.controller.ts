dfcec1492d68fbf9836cd16bdae2aff1041dd283
kthss01 <kthss01@naver.com>
2023-10-05 21:10:16 +0900

    비즈니스 체크 로직 서비스 단으로 이동

--- src/taxi/taxi.controller.ts
+++ src/taxi/taxi.controller.ts
@@ -1,66 +1,38 @@
 import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TaxiService } from './taxi.service';
 
 @Controller('taxi')
 export class TaxiController {
+  constructor(private readonly taxiService: TaxiService) {}
+
   // 전체 택시 정보 조회
   @Get()
   getAllTaxiInfo(): string {
-    // 사용자 권한 확인
-
-    return 'return all taxi info';
+    return this.taxiService.getAllTaxiInfo();
   }
 
   // 택시 Id로 택시 정보 조회
   @Get(':id')
   getTaxiInfoById(@Param('id') taxiId: string): string {
-    // 사용자 권한 확인
-    // 택시 id 존재 확인
-
-    return `return taxi info by id : ${taxiId}`;
+    return this.taxiService.getTaxiInfoById(taxiId);
   }
 
   // 택시 정보 등록
+  // TODO: 파라미터 받아야함
   @Post()
   createTaxiInfo(): string {
-    // 사용자 권한 확인
-    /**
-     * 파라미터 확인
-     *  드라이버
-     *  => 드라이버 존재 확인
-     *  택시 종류
-     *  => 택시 종류 확인
-     *  차량 번호
-     *  => 차량 번호 확인
-     */
-    // 택시 id 생성
-
-    return 'create taxi info';
+    return this.taxiService.createTaxiInfo();
   }
 
   // 택시 정보 수정
   @Patch(':id')
   updateTaxiInfoById(@Param('id') taxiId: string): string {
-    // 사용자 권한 확인
-    /**
-     * 파라미터 확인
-     * 드라이버
-     *  => 드라이버 존재 확인
-     *  택시 종류
-     *  => 택시 종류 확인
-     *  차량 번호
-     *  => 차량 번호 확인
-     */
-    // 택시 id 존재 확인
-
-    return `update taxi info by id : ${taxiId}`;
+    return this.taxiService.updateTaxiInfoById(taxiId);
   }
 
   // 택시 정보 삭제
   @Delete(':id')
   deleteTaxiInfoById(@Param('id') taxiId: string): string {
-    // 사용자 권한 확인
-    // 택시 id 존재 확인
-
-    return `delete taxi info by id : ${taxiId}`;
+    return this.taxiService.deleteTaxiInfoById(taxiId);
   }
 }
--- src/taxi/taxi.service.ts
+++ src/taxi/taxi.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TaxiService {}
+export class TaxiService {
+  // 전체 택시 정보 조회
+  getAllTaxiInfo(): string {
+    // 사용자 권한 확인
+    return 'return all taxi info';
+  }
+
+  // 택시 Id로 택시 정보 조회
+  getTaxiInfoById(taxiId: string): string {
+    // 사용자 권한 확인
+    // 택시 id 존재 확인
+    return `return taxi info by id : ${taxiId}`;
+  }
+
+  // 택시 정보 등록
+  createTaxiInfo(): string {
+    // 사용자 권한 확인
+    /**
+     * 파라미터 확인
+     *  드라이버
+     *  => 드라이버 존재 확인
+     *  택시 종류
+     *  => 택시 종류 확인
+     *  차량 번호
+     *  => 차량 번호 확인
+     */
+    // 택시 id 생성
+    return 'create taxi info';
+  }
+
+  // 택시 정보 수정
+  updateTaxiInfoById(taxiId: string): string {
+    // 사용자 권한 확인
+    /**
+     * 파라미터 확인
+     * 드라이버
+     *  => 드라이버 존재 확인
+     *  택시 종류
+     *  => 택시 종류 확인
+     *  차량 번호
+     *  => 차량 번호 확인
+     */
+    // 택시 id 존재 확인
+
+    return `update taxi info by id : ${taxiId}`;
+  }
+
+  // 택시 정보 삭제
+  deleteTaxiInfoById(taxiId: string): string {
+    // 사용자 권한 확인
+    // 택시 id 존재 확인
+    return `delete taxi info by id : ${taxiId}`;
+  }
+}
