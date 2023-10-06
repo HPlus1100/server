e51714464441aa92b55ab2aa25bd4ef05946467f
kthss01 <kthss01@naver.com>
2023-10-07 00:24:44 +0900

    typemodule typeorm 설정 추가

--- src/taxi/taxi.module.ts
+++ src/taxi/taxi.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TaxiController } from './taxi.controller';
 import { TaxiService } from './taxi.service';
+import { TaxiRepository } from './taxi.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaxiRepository])],
   controllers: [TaxiController],
-  providers: [TaxiService],
+  providers: [TaxiService, TaxiRepository],
 })
 export class TaxiModule {}
