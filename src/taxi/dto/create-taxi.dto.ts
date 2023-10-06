c37ce655f5c31951b66b32640d7ad82aaca3f5d8
kthss01 <kthss01@naver.com>
2023-10-06 23:38:10 +0900

    create taxi dto NN 처리

--- src/taxi/dto/create-taxi.dto.ts
+++ src/taxi/dto/create-taxi.dto.ts
@@ -1,18 +1,23 @@
-import { IsInt, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTaxiDto {
+  @IsNotEmpty()
   @IsInt()
   driver_no: number;
 
+  @IsNotEmpty()
   @IsInt()
   type: string;
 
+  @IsNotEmpty()
   @IsString()
   company_name: string;
 
+  @IsNotEmpty()
   @IsString()
   car_num: string;
 
+  @IsNotEmpty()
   @IsString()
   car_model: string;
 }
