795f8b2c61216c2c48c0bfa61fe1ffc305728742
kthss01 <kthss01@naver.com>
2023-10-07 17:32:58 +0900

    taxi repository 추가

--- src/taxi/taxi.module.ts
+++ src/taxi/taxi.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaxiController } from './taxi.controller';
 import { TaxiService } from './taxi.service';
+import { TaxiRepository } from './taxi.repository';
 
 @Module({
   controllers: [TaxiController],
-  providers: [TaxiService],
+  providers: [TaxiService, TaxiRepository],
 })
 export class TaxiModule {}
