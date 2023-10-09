7f368c2ca5d288efaceb36e497bb1efd875bf00f
qk1413 <qk1413@naver.com>
2023-10-09 21:16:44 +0900

    feature: billing service processPayment 주석

--- src/billing/billing.service.ts
+++ src/billing/billing.service.ts
@@ -12,23 +12,23 @@ export class BillingService {
     private readonly dailyEarningsRepository: DailyEarningsRepository,
   ) {}
 
-  async processPayment(
-    paymentInfo: PaymentInfoDto,
-    amount: number,
-  ): Promise<string> {
-    console.log(paymentInfo);
-    console.log(amount);
-    /**
-     *
-     * 1. 결제 정보 및 금액 확인
-     * 2. 해당하는 경우 결제 API를 사용하여 결제 처리
-     * 3. 결제 내역 반환
-     */
-    return;
-  }
+  // async processPayment(
+  //   paymentInfo: PaymentInfoDto,
+  //   amount: number,
+  // ): Promise<string> {
+  //   console.log(paymentInfo);
+  //   console.log(amount);
+  //   /**
+  //    *
+  //    * 1. 결제 정보 및 금액 확인
+  //    * 2. 해당하는 경우 결제 API를 사용하여 결제 처리
+  //    * 3. 결제 내역 반환
+  //    */
+  //   return;
+  // }
 
   async saveUserPaymentInfo(paymentInfo: PaymentInfoDto): Promise<string> {
-    const result = await this.paymentRepository.save(paymentInfo);
+    await this.paymentRepository.save(paymentInfo);
 
     // this.paymentRepository.save(paymentInfo);
     /**
@@ -36,18 +36,16 @@ export class BillingService {
      * 2. 데이터베이스에 결제 정보 저장
      * 3. 저장 상태 반환
      */
-    console.log(result);
-    return;
+    return ;
   }
 
   async getPaymentInfo(userId: string): Promise<PaymentInfoDto> {
-    const result = await this.paymentRepository.findByPaymentInfo(userId);
+    await this.paymentRepository.findByPaymentInfo(userId);
     /**
      * 1. 사용자 유효성 검사
      * 2. 데이터베이스에서 결제 정보 검색
      * 3. 결제 정보 반환
      */
-    console.log(result);
     return;
   }
 
