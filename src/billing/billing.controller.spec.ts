788d625e43b7f2b42da51b16ab01541678f84338
june0929 <qk1413@naver.com>
2023-10-14 04:19:43 +0900

    test: billing controller 테스트 코드 작성(결제정보저장,결제정보 조회, 일일수익 조회)

--- src/billing/billing.controller.spec.ts
+++ src/billing/billing.controller.spec.ts
@@ -1,14 +1,10 @@
-import {Test, TestingModule} from '@nestjs/testing';
-import {BillingController} from './billing.controller';
-import {BillingService} from './billing.service';
-import {getRepositoryToken} from "@nestjs/typeorm";
-import { Billing } from "@billing/domain/entity/billing.entity";
-import { DailyEarnings } from "@billing/domain/entity/daily-earning.entity";
-import { PaymentMethod } from "@billing/domain/entity/payment-method.entity";
-import { PaymentInfoDto } from "@billing/domain/dto/payment-info.dto";
-import { PaymentType } from "@billing/domain/type/billing.enum";
-import {PaymentRepository} from "@billing/repository/payment.repository";
-import {DailyEarningsRepository} from "@billing/repository/daily-earning.repository";
+import { Test, TestingModule } from '@nestjs/testing';
+import { BillingController } from './billing.controller';
+import { BillingService } from './billing.service';
+import { PaymentInfoDto } from '@billing/domain/dto/payment-info.dto';
+import { PaymentType } from '@billing/domain/type/billing.enum';
+import { PaymentRepository } from '@billing/repository/payment.repository';
+import { DailyEarningsRepository } from '@billing/repository/daily-earning.repository';
 
 const createMockRepository = jest.fn(() => ({
   save: jest.fn(),
@@ -31,7 +27,9 @@ describe('BillingController', () => {
       ],
     }).compile();
 
-    controller = module.get<BillingController>(BillingController) as BillingController;
+    controller = module.get<BillingController>(
+      BillingController,
+    ) as BillingController;
     service = module.get<BillingService>(BillingService) as BillingService;
   });
 
@@ -49,22 +47,22 @@ describe('BillingController', () => {
       createSpy.mockReset();
     });
     it('should call the service method and return a success string', async () => {
-      const mockPaymentInfoDto: PaymentInfoDto  = {
+      const mockPaymentInfoDto: PaymentInfoDto = {
         customerNo: '12345',
         type: PaymentType.Card,
         cardNum: '1234567812345678',
         cvc: '123',
         expireDate: '2025-12-01',
         isDefault: true,
-        isDeleted: false
+        isDeleted: false,
       };
       createSpy.mockReturnValue(mockPaymentInfoDto);
 
       const result = controller.savePaymentInfo(mockPaymentInfoDto);
-      expect(result).toEqual("PaymentInfo save controller");
+      expect(result).toEqual('PaymentInfo save controller');
     });
   });
-  
+
   describe('selectPaymentInfo -> findOne', () => {
     let selectSpy: jest.SpyInstance;
     beforeEach(() => {
@@ -73,16 +71,15 @@ describe('BillingController', () => {
     afterEach(() => {
       selectSpy.mockReset();
     });
-    
+
     it('should call the service method and return a success string', async () => {
       const userId = 'testUser';
       const expectedMessage = `welcome! : ${userId} This service select your PaymentInfo`;
 
       selectSpy.mockReturnValue(userId);
-      
+
       const result = controller.selectPaymentInfo(userId);
       expect(result).toEqual(expectedMessage);
-
     });
   });
 
@@ -95,20 +92,24 @@ describe('BillingController', () => {
     afterEach(() => {
       selectSpy.mockReset();
     });
-    
+
     it('should call the service method and return the service result with a welcome message', async () => {
       const userId = 'testUser';
       const earningDate = '2023-10-01,2023-10-02';
-      const mockEarnings = { userId: 'testUser', earningDate:'2023-10-01,2023-10-02' };
+      const mockEarnings = {
+        userId: 'testUser',
+        earningDate: '2023-10-01,2023-10-02',
+      };
 
       selectSpy.mockReturnValue(mockEarnings);
 
-      const result = await controller.getEarningsToday(userId,earningDate);
+      const result = await controller.getEarningsToday(userId, earningDate);
 
-      const expectedMessage = `welcome! : ${userId} This service select your DailyEarnings \n result : ` + JSON.stringify(mockEarnings);
+      const expectedMessage =
+        `welcome! : ${userId} This service select your DailyEarnings \n result : ` +
+        JSON.stringify(mockEarnings);
 
       expect(result).toEqual(expectedMessage);
-
     });
   });
 });
