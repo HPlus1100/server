b8c15a4acd1721035cd3831f725d2a9810dc35ae
SeungBin Kim <luxorienskim@gmail.com>
2023-10-13 10:51:36 +0900

    feat: implement min to run findAll payments test

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { Payment } from '@payments/entities/payment.entity';
 
 @Injectable()
 export class PaymentsService {
@@ -8,8 +9,8 @@ export class PaymentsService {
     return 'This action adds a new payment';
   }
 
-  findAll() {
-    return `This action returns all payments`;
+  findAll(): Payment[] {
+    return [];
   }
 
   findOne(id: number) {
