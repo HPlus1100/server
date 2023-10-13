8b6639af4bfd4e67aa89f6662646154cf55b0e03
june0929 <qk1413@naver.com>
2023-10-14 04:21:18 +0900

    refactor: billing controller,service 필요없는 주석 제거

--- src/billing/billing.controller.ts
+++ src/billing/billing.controller.ts
@@ -8,7 +8,8 @@ export class BillingController {
 
   /**
    * 결제 EndPoint
-   *
+   * ->보류 : 결제정보를 찾은 후 payment-API사용이라 생각했으나, 단순 DB CRUD로 변경.
+   * -> findOne
    * @param {paymentInfo: PaymentInfo, amount: number} - 결제정보, 결제금액
    * @returns {status: string; amount: number; ...otherData} - 결제결과, 처리된금x`액
    */
@@ -17,36 +18,18 @@ export class BillingController {
     const result = this.billingService.findByBankAccountNumber(
       paymentInfo['accountNumber'],
     );
-    /**
-     * 의문 1. 결제 관련 로직을 비동기로 처리하는게 옳은 것인가..?
-     * -> 결제 요청같은경우, 확장성 및 UX 측면에서는 비동기가맞으나, 실제 금액이 처리되는 앱은 아니다보니, 고민을 해봐야 할 것 같음.
-     *
-     * 구현
-     * -> 매개변수 DTO사용
-     * -> 결제 정보, 금액 확인 및 유효성 검사. (금액적인 부분은 유효성검사가 있다면 좋을것같다 생각)
-     * -> 유저 확인(결제 요청을 한 유저)
-     *
-     * -> 금액 및 유저 확인 후 결제 API를 위한 Service로 전달하거나 실패 메시지 전달.
-     */
     return result;
   }
 
   /**
-   * 청구(결제) 수단 저장 EndPoint
+   * 청구(결제) 수단 저장 EndPoint -> create
    * @param {PaymentInfo} paymentInfo - 결제 수단 정보
    * @returns {status: string} - 정보 저장 상태를 반환.
+   * create
    */
   @Post('payment-info')
   savePaymentInfo(@Body() PaymentInfoDto: PaymentInfoDto) {
     this.billingService.saveUserPaymentInfo(PaymentInfoDto);
-    /**
-     * 구현
-     * -> 매개변수 DTO사용
-     * -> 유저 확인
-     * -> 필수 필드값이 존재하는지 여부 확인. 없다면 반환.
-     * -> 유저 및 필드값 유효성 검사 확인 한다면 service 계층으로 전달.
-     */
-
     return 'PaymentInfo save controller';
   }
 
@@ -54,16 +37,11 @@ export class BillingController {
    * 카드/계좌 정보 조회 EndPoint
    * @param {string} userId - 유저 id
    * @returns {PaymentInfo} - 조회한 카드 또는 계좌 정보를 반환.
+   * create
    */
   @Get('payment-info/:userId')
   selectPaymentInfo(@Param('userId') userId: string) {
     this.billingService.getPaymentInfo(userId);
-    /**
-     * 구현
-     * -> 유저 유효성 검사
-     * -> 인증된 사용자면 service계층전달.
-     * -> 인증되지않았다면 실패
-     */
     return `welcome! : ${userId} This service select your PaymentInfo`;
   }
 
@@ -72,6 +50,7 @@ export class BillingController {
    * @param {string} userId - 유저 id
    * @Param {Date} earningsDates - 조회 날짜
    * @returns {DailyEarnings} - 조회한 당일 수익 정보
+   * -> selectMany
    */
   @Get('earnings-today/:userId')
   async getEarningsToday(
@@ -82,12 +61,6 @@ export class BillingController {
       userId,
       earningsDates,
     );
-    /**
-     * 구현
-     * -> 유저 유효성 검사
-     * -> 인증된 사용자-> service계층 전달.
-     * -> 인증되지 않았다면 실패.
-     */
     return (
       `welcome! : ${userId} This service select your DailyEarnings \n result : ` +
       JSON.stringify(result)
--- src/billing/billing.service.ts
+++ src/billing/billing.service.ts
@@ -4,6 +4,7 @@ import { DailyEarningDTO } from './domain/dto/daily-earning.dto';
 import { PaymentRepository } from './repository/payment.repository';
 import { DailyEarningsRepository } from './repository/daily-earning.repository';
 import { format } from 'date-fns';
+import { PaymentMethod } from '@billing/domain/entity/payment-method.entity';
 
 @Injectable()
 export class BillingService {
@@ -12,41 +13,14 @@ export class BillingService {
     private readonly dailyEarningsRepository: DailyEarningsRepository,
   ) {}
 
-  // async processPayment(
-  //   paymentInfo: PaymentInfoDto,
-  //   amount: number,
-  // ): Promise<string> {
-  //   console.log(paymentInfo);
-  //   console.log(amount);
-  //   /**
-  //    *
-  //    * 1. 결제 정보 및 금액 확인
-  //    * 2. 해당하는 경우 결제 API를 사용하여 결제 처리
-  //    * 3. 결제 내역 반환
-  //    */
-  //   return;
-  // }
-
-  async saveUserPaymentInfo(paymentInfo: PaymentInfoDto): Promise<string> {
-    await this.paymentRepository.save(paymentInfo);
-
-    // this.paymentRepository.save(paymentInfo);
-    /**
-     * 1. 결제 정보 확인
-     * 2. 데이터베이스에 결제 정보 저장
-     * 3. 저장 상태 반환
-     */
-    return ;
+  async saveUserPaymentInfo(
+    paymentInfo: PaymentInfoDto,
+  ): Promise<PaymentInfoDto> {
+    return await this.paymentRepository.save(paymentInfo);
   }
 
-  async getPaymentInfo(userId: string): Promise<PaymentInfoDto> {
-    await this.paymentRepository.findByPaymentInfo(userId);
-    /**
-     * 1. 사용자 유효성 검사
-     * 2. 데이터베이스에서 결제 정보 검색
-     * 3. 결제 정보 반환
-     */
-    return;
+  async getPaymentInfo(userId: string): Promise<PaymentMethod> {
+    return await this.paymentRepository.findByPaymentInfo(userId);
   }
 
   /**
