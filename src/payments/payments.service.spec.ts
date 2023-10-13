3d9c1d60abcda242218ef0fd7e58ba1473c6fbfd
SeungBin Kim <luxorienskim@gmail.com>
2023-10-13 10:37:40 +0900

    test: add test suites for payment service

--- src/payments/payments.service.spec.ts
+++ src/payments/payments.service.spec.ts
@@ -15,4 +15,30 @@ describe('PaymentsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    describe('when getting all payments', () => {
+      it.todo('should return an array of payments');
+    });
+  });
+  describe('findOne', () => {
+    describe('', () => {
+      it.todo('should return a payment');
+    });
+  });
+  describe('create', () => {
+    describe('when creating a payment', () => {
+      it.todo('should return the created payment id');
+    });
+  });
+  describe('update', () => {
+    describe('when updating a payment', () => {
+      it.todo('should return true if the payment is updated');
+    });
+  });
+  describe('remove', () => {
+    describe('when removing a payment', () => {
+      it.todo('should return true if the payment is removed');
+    });
+  });
 });
