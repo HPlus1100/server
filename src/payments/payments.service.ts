c360d550651f7bbbb5e242ea6748a40b467fe92b
SeungBin Kim <luxorienskim@gmail.com>
2023-10-13 11:47:49 +0900

    feat: implement findAll payments service

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { Payment } from '@payments/entities/payment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PaymentsService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
+  ) {
+  }
+  
   create(createPaymentDto: CreatePaymentDto) {
     return 'This action adds a new payment';
   }
 
-  findAll(): Payment[] {
-    return [];
+  findAll(): Promise<Payment[]> {
+    return this.paymentRepository.find({});
   }
 
   findOne(id: number) {
