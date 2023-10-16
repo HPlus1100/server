33c12b61b3756eeaa434833ecebfa2b6cae30454
junman <junman9595@gmail.com>
2023-10-16 21:54:54 +0900

    feat: createCall 성공 케이스 테스트 코드 수정

--- src/call/call.service.spec.ts
+++ src/call/call.service.spec.ts
@@ -3,6 +3,10 @@ import { CallService } from './call.service';
 import { CallRepository } from './call.repository';
 import { DataSource } from 'typeorm';
 import { setupDataSource } from '../../test/util/setupDataSource';
+import { Call } from './call.entity';
+import { PathApiRepository } from '@/externalApi/path-api.repository';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('CallService', () => {
   let service: CallService;
@@ -11,9 +15,11 @@ describe('CallService', () => {
   beforeEach(async () => {
     dataSource = await setupDataSource();
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule.forRoot()],
       providers: [
         CallService,
         CallRepository,
+        PathApiRepository,
         { provide: DataSource, useValue: dataSource },
       ],
     }).compile();
@@ -26,13 +32,23 @@ describe('CallService', () => {
   });
 
   describe('createCall', () => {
-    it('call은 유저 아이디 1을 가지고 있다.', async () => {
+    it('호출 생성의 결과 값은 Call entity 형태여야 한다.', async () => {
       const call = await service.createCall({
         userId: '1',
         taxiType: 'DELUXE',
+        departure: {
+          address: '서울 마포구 독막로 266',
+          lat: 126.942389642584,
+          lng: 37.5438675433938,
+        },
+        arrival: {
+          address: '경기도 성남시 분당구 성남대로 55',
+          lat: 127.108947909824,
+          lng: 37.3399767000124,
+        },
       });
 
-      expect(call.customerNo).toBe('1');
+      expect(call).toBeInstanceOf(Call);
     });
   });
 });
