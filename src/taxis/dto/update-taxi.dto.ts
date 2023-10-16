81b962d0281127e8f48dc19c2efe0bfe53a12130
kthss01 <kthss01@naver.com>
2023-10-16 22:04:08 +0900

    refactor: update-taxi.dto

--- src/taxis/dto/update-taxi.dto.ts
+++ src/taxis/dto/update-taxi.dto.ts
@@ -1,18 +1,4 @@
-import { IsInt, IsString } from 'class-validator';
-import { CarType } from '../types/taxi.enum';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTaxiDto } from './create-taxi.dto';
 
-export class UpdateTaxiDto {
-  @IsInt()
-  driverLicenseNumber: number;
-
-  carType: CarType;
-
-  @IsString()
-  companyName: string;
-
-  @IsString()
-  licensePlateNumber: string;
-
-  @IsString()
-  carModel: string;
-}
+export class UpdateTaxiDto extends PartialType(CreateTaxiDto) {}
