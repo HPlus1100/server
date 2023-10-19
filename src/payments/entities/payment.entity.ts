c5c7ecb7b6215427b9cf8349b58813a891648456
SeungBin Kim <luxorienskim@gmail.com>
2023-10-19 10:58:26 +0900

    feat: PrimaryGeneratedColumn in payment entity

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,4 +1,7 @@
-import { Entity } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('payments')
-export class Payment {}
+export class Payment extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  no: string;
+}
