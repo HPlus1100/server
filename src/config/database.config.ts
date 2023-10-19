901a3b238f18f25e2952930abc057b0959fafe5e
SeungBin Kim <luxorienskim@gmail.com>
2023-10-19 17:59:49 +0900

    feat: database configuration
    As running database in local environment has no secret value, all environment variables in this namespace has default value. And the default value is not declared using Joi because then Nest can't infer types

---
+++ src/config/database.config.ts
@@ -0,0 +1,42 @@
+import { registerAs } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+export default registerAs('database', async () => {
+  const loadedValues = {
+    host: process.env.DB_HOST || 'localhost',
+    port: +process.env.DB_PORT || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_DATABASE || 'taxiDB',
+  };
+
+  const baseSchema = Joi.object({
+    host: Joi.alternatives([
+      Joi.string().valid('localhost'),
+      Joi.string().regex(/^[a-z0-9-]+\\.([a-z0-9-]+)\\.(ap-northeast-2)\\.rds\\.amazonaws\\.com$/),
+    ]).required(),
+    port: Joi.number().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    database: Joi.string().required(),
+  });
+
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      const productionSchema = baseSchema.keys({
+        host: Joi.invalid('localhost'),
+        username: Joi.invalid('postgres'),
+        password: Joi.invalid('postgres'),
+      });
+
+      await productionSchema.validateAsync(loadedValues, { abortEarly: false });
+    }
+
+    await baseSchema.validateAsync(loadedValues, { abortEarly: false });
+
+  } catch (e) {
+    throw new Error(e);
+  }
+
+  return loadedValues;
+});
