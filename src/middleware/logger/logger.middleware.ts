01d40b83f3e4669de28292c60ea8fafd3df487ab
SeungBin Kim <luxorienskim@gmail.com>
2023-10-20 12:58:32 +0900

    feat: logger middleware
    Basic implementation of logger middleware

--- src/middleware/logger/logger.middleware.ts
+++ src/middleware/logger/logger.middleware.ts
@@ -3,6 +3,8 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    // todo implement custom logger
+    console.log('Request...');
     next();
   }
 }
