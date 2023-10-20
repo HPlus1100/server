ef851cf440811b953d7fb5a11e6b7065b893f90b
june0929 <qk1413@naver.com>
2023-10-20 17:16:32 +0900

    fix: logger conf 분리 및 환경별 설정

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,32 +1,39 @@
-import winston from 'winston';
-import WinstonCloudWatch from 'winston-cloudwatch';
+import { createLogger, format, transports, Logger } from 'winston';
+import * as appRoot from 'app-root-path';
 
-// Winston 로거 인스턴스를 생성합니다.
-const logger = winston.createLogger({
-  level: 'info', // 로그 레벨 설정
-  format: winston.format.combine(
-    winston.format.timestamp(), // 타임스탬프 포맷
-    winston.format.json(), // 로그를 JSON 형태로 출력
-    winston.format.colorize(), // 로그에 색상 추가
-  ),
-  transports: [
-    new winston.transports.Console(),
-  ],
-});
-
-// 프로덕션 환경에서 CloudWatch 로깅을 추가합니다.
-if (process.env.NODE_ENV === 'production') {
-  const cloudwatchConfig = {
-    logGroupName: process.env.CLOUDWATCH_GROUP_NAME, // 환경 변수에서 로그 그룹 이름을 가져옵니다.
-    logStreamName: `${process.env.CLOUDWATCH_GROUP_NAME}-${process.env.NODE_ENV}`, // 로그 스트림 이름을 설정합니다.
-    awsAccessKeyId: process.env.CLOUDWATCH_ACCESS_KEY, // AWS 접근 키를 설정합니다.
-    awsSecretKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY, // AWS 비밀 키를 설정합니다.
-    awsRegion: process.env.CLOUDWATCH_REGION, // AWS 리전을 설정합니다.
-    messageFormatter: ({ level, message, additionalInfo }) => `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(additionalInfo)}}`, // 로그 메시지 형식을 설정합니다.
+export function createAppLogger(): Logger {
+  const options = {
+    file: {
+      level: 'info',
+      filename: `${appRoot}/logs/app.log`,
+      handleExceptions: true,
+      format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.json(),
+      ),
+    },
+    console: {
+      level: 'debug',
+      handleExceptions: true,
+      format: format.combine(
+        format.colorize(),
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+        ),
+      ),
+    },
   };
 
-  // CloudWatch 전송을 위한 새로운 Winston CloudWatch 인스턴스를 추가합니다.
-  logger.add(new WinstonCloudWatch(cloudwatchConfig));
-}
+  // 환경별 로그 설정
+  const logger = createLogger({
+    transports: [new transports.File(options.file)],
+  });
 
-export default logger; // 이 로거를 다른 파일에서 사용할 수 있도록 내보냅니다.
+  // 개발 환경에서는 콘솔에도 로그 출력
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new transports.Console(options.console));
+  }
+
+  return logger;
+}
