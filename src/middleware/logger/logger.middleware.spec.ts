361d14007bf3212260bde97e1a62e2aa03aa0611
SeungBin Kim <luxorienskim@gmail.com>
2023-10-20 12:48:11 +0900

    test: add basic test for logger middleware

--- src/middleware/logger/logger.middleware.spec.ts
+++ src/middleware/logger/logger.middleware.spec.ts
@@ -1,7 +1,23 @@
 import { LoggerMiddleware } from './logger.middleware';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('LoggerMiddleware', () => {
-  it('should be defined', () => {
-    expect(new LoggerMiddleware()).toBeDefined();
+  let middleware: LoggerMiddleware;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LoggerMiddleware],
+    }).compile();
+    middleware = module.get<LoggerMiddleware>(LoggerMiddleware);
+  });
+
+  it('should call the next function', () => {
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    middleware.use(req, res, next);
+
+    expect(next).toHaveBeenCalled();
   });
 });
