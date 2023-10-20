d02b281c05efbcc27c81598e765ba550878cdc59
SeungBin Kim <luxorienskim@gmail.com>
2023-10-20 11:58:40 +0900

    feat: add consoleTransportOptions for development env
    It adds colors and timestamps for local development.
    Also added format.simple() to all envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { createLogger, transports } from 'winston';
+import { createLogger, format, transports } from 'winston';
 import { WinstonModule } from 'nest-winston';
+import { ConsoleTransportOptions } from 'winston/lib/winston/transports';
 
 interface EnvironmentVariables {
   PORT: number;
@@ -11,7 +12,11 @@ interface EnvironmentVariables {
 async function bootstrap() {
   // todo refactor & move code to a separate file
   // todo configure according to NODE_ENV
-  const instance = createLogger({ transports: [new transports.Console()] });
+  const consoleTransportOptions: ConsoleTransportOptions = {
+    handleExceptions: true,
+    format: process.env.NODE_ENV === 'development' ? format.combine(format.colorize(), format.timestamp(), format.simple()) : format.simple(),
+  };
+  const instance = createLogger({ transports: [new transports.Console(consoleTransportOptions)] });
   const logger = WinstonModule.createLogger({ instance });
 
   const app = await NestFactory.create(AppModule, { logger });
